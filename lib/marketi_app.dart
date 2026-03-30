423a465e05949b82588df79bea5a36a4cae0793b
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-03-30 07:21:16 +0200

    refactor: extract MyApp to separate file and rename to MarketiApp

---
+++ lib/marketi_app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MarketiApp extends StatelessWidget {
+  const MarketiApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(debugShowCheckedModeBanner: false);
+  }
+}
