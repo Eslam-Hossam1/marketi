fe1e456448758d7588cb5485cb69b4ed3c61f65e
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-03-30 07:03:26 +0200

    feat(core): add app regex and form validators

---
+++ lib/core/theme/utils/app_regex.dart
@@ -0,0 +1,43 @@
+class AppRegex {
+  static bool isValidEmail(String email) {
+    return RegExp(r'^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$').hasMatch(email);
+  }
+
+  static bool isPasswordValid(String password) {
+    return RegExp(
+      r"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$",
+    ).hasMatch(password);
+  }
+
+  static bool hasNumberOrSpecialCharacter(String password) {
+    return RegExp(r'^(?=.*?[0-9#?!@$%^&*-])').hasMatch(password);
+  }
+
+  static bool isPhoneNumberValid(String phoneNumber) {
+    return RegExp(r'^(010|011|012|015)[0-9]{8}$').hasMatch(phoneNumber);
+  }
+
+  static bool hasLowerCase(String password) {
+    return RegExp(r'^(?=.*[a-z])').hasMatch(password);
+  }
+
+  static bool hasUpperCase(String password) {
+    return RegExp(r'^(?=.*[A-Z])').hasMatch(password);
+  }
+
+  static bool hasNumber(String password) {
+    return RegExp(r'^(?=.*?[0-9])').hasMatch(password);
+  }
+
+  static bool hasSpecialCharacter(String password) {
+    return RegExp(r'^(?=.*?[#?!@$%^&*_\-])').hasMatch(password);
+  }
+
+  static bool hasMinLength(String password) {
+    return RegExp(r'^(?=.{8,})').hasMatch(password);
+  }
+
+  static bool isNumeric(String text) {
+    return RegExp(r'^[0-9]+$').hasMatch(text);
+  }
+}
---
+++ lib/core/theme/utils/form_validators.dart
@@ -0,0 +1,61 @@
+import 'package:marketi/core/theme/utils/app_regex.dart';
+
+abstract class FormValidators {
+  static String? customTextFormFieldValidator(String? value) {
+    if (isValueEmpty(value)) {
+      return 'This field is required';
+    } else {
+      return null;
+    }
+  }
+
+  static String? emailValidator(String? value) {
+    if (isValueEmpty(value)) {
+      return 'This field is required';
+    }
+    if (!AppRegex.isValidEmail(value!)) {
+      return 'Invalid email address';
+    }
+    return null;
+  }
+
+  static String? passwordValidator(String? value) {
+    if (value == null || value.isEmpty) {
+      return 'Please enter your password';
+    }
+    if (value.length < 6) {
+      return 'Password must be at least 6 characters long';
+    }
+    if (!AppRegex.hasLowerCase(value)) {
+      return 'Password must contain at least one lowercase letter';
+    }
+    if (!AppRegex.hasUpperCase(value)) {
+      return 'Password must contain at least one uppercase letter';
+    }
+    if (!AppRegex.hasNumber(value)) {
+      return 'Password must contain at least one number';
+    }
+    if (!AppRegex.hasSpecialCharacter(value)) {
+      return 'Password must contain at least one special character';
+    }
+    return null;
+  }
+
+
+  static String? pincodeTextFormFieldValidator(String? value) {
+    if (isValueEmpty(value) || value!.length < 4) {
+      return 'Please enter the correct code';
+    } else {
+      return null;
+    }
+  }
+
+  static bool isValueEmpty(String? value) {
+    if (value == null || value.isEmpty) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+
+}
