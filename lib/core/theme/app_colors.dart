6ffa368dd31e4ca07a63dc0dec051eebb2ed3486
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-03-30 06:27:35 +0200

    feat(core): setup app themes and text styles

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+abstract class AppColors {
+  // Light Theme Colors
+  static const Color lightPrimary = Color(0xFF3F80FF);
+  static const Color lightSecondary = Color(0xFFFE0017);
+  static const Color lightOnSurfaceVariant = Color(0xFF9FA5C0);
+  static const Color lightOutline = Color(0xFFb2ccff);
+  static const Color lightScaffoldBackground = Color(0xFFffffff);
+  static const Color lightDialogBackground = Color(0xFFF0F0F0);
+  static const Color lightMainTextColor = Color(0xFF001640);
+  static const Color lightSecondaryTextColor = Color(0xFF929bab);
+  static const Color lightFormColor = Color(0xFFF3F6F6);
+  static const Color lightToastBackground = Color(0xFFF4F5F7);
+  static const Color lightCachedNetworkImagePlacholderColor = Color(0xFFEBEBF4);
+  // 💬 Chat Bubble Colors (Light)
+  static const Color lightMyChatBubbleBackground = Color(0xFF20A090);
+  static const Color lightOtherChatBubbleBackground = Color(0xFFE5F6FD);
+  static const Color lightMyChatBubbleTextColor = Color(0xFFFFFFFF);
+  static const Color lightOtherChatBubbleTextColor = Color(0xFF0B0B0B);
+  static const Color lightMyChatBubbleTimeTextColor = Color(0xFFD1F5FF);
+  static const Color lightOtherChatBubbleTimeTextColor = Color(0xFF7D869C);
+
+  // 💬 Chat Bubble Colors (Dark)
+  static const Color darkMyChatBubbleBackground = Color(0xFF1E8C6C);
+  static const Color darkOtherChatBubbleBackground = Color(0xFF1B2735);
+  static const Color darkMyChatBubbleTextColor = Color(0xFFFFFFFF);
+  static const Color darkOtherChatBubbleTextColor = Color(0xFFEDEDED);
+  static const Color darkMyChatBubbleTimeTextColor = Color(0xFFC8F1FF);
+  static const Color darkOtherChatBubbleTimeTextColor = Color(0xFF9FA9C1);
+
+  // Dark Theme Colors
+  static const Color darkPrimary = Color(0xFF3F80FF);
+  static const Color darkSecondary = Color(0xFFFE0017);
+  static const Color darkOnSurfaceVariant = Color(0xFFB0BEC5);
+  static const Color darkSurface = Color(0xFFA9A9A9);
+  static const Color darkOutline = Color(0xFF3C3C3C);
+  static const Color darkScaffoldBackground = Color(0xFF121212);
+  static const Color darkDialogBackground = Color(0xFF1E1E1E);
+  static const Color darkMainTextColor = Color(0xFFECEFF4);
+  static const Color darkSecondaryTextColor = Color(0xFFB0BEC5);
+  static const Color darkFormColor = Color(0xFF2E2E2E);
+  static const Color darkToastBackground = Color(0xFF2E2E2E);
+  static const Color darkCachedNetworkImagePlacholderColor = Color(0xFF3A3A3A);
+}
