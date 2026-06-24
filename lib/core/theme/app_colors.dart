4848576ffe32cc9cc9ca746cbcfefd3bb7d692df
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-06-24 20:12:28 +0300

    change NextCart logo color

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -2,13 +2,13 @@ import 'package:flutter/material.dart';
 
 abstract class AppColors {
   // Light Theme Colors
-  static const Color lightPrimary = Color(0xFF3F80FF);
-  static const Color lightSecondary = Color(0xFFFE0017);
+  static const Color lightPrimary = Color(0xFFE8000D);
+  static const Color lightSecondary = Color(0xFFB20009);
   static const Color lightOnSurfaceVariant = Color(0xFF9FA5C0);
-  static const Color lightOutline = Color(0xFFb2ccff);
+  static const Color lightOutline = Color(0xFFFFBABA);
   static const Color lightScaffoldBackground = Color(0xFFffffff);
   static const Color lightDialogBackground = Color(0xFFF0F0F0);
-  static const Color lightMainTextColor = Color(0xFF001640);
+  static const Color lightMainTextColor = Color(0xFF1A0000);
   static const Color lightSecondaryTextColor = Color(0xFF929bab);
   static const Color lightFormColor = Color(0xFFF3F6F6);
   static const Color lightToastBackground = Color(0xFFF4F5F7);
@@ -30,14 +30,14 @@ abstract class AppColors {
   static const Color darkOtherChatBubbleTimeTextColor = Color(0xFF9FA9C1);
 
   // Dark Theme Colors
-  static const Color darkPrimary = Color(0xFF3F80FF);
-  static const Color darkSecondary = Color(0xFFFE0017);
+  static const Color darkPrimary = Color(0xFFDB3022);
+  static const Color darkSecondary = Color(0xFFB20009);
   static const Color darkOnSurfaceVariant = Color(0xFFB0BEC5);
   static const Color darkSurface = Color(0xFFA9A9A9);
   static const Color darkOutline = Color(0xFF3C3C3C);
   static const Color darkScaffoldBackground = Color(0xFF121212);
   static const Color darkDialogBackground = Color(0xFF1E1E1E);
-  static const Color darkMainTextColor = Color(0xFFECEFF4);
+  static const Color darkMainTextColor = Color(0xFFFFF5F5);
   static const Color darkSecondaryTextColor = Color(0xFFB0BEC5);
   static const Color darkFormColor = Color(0xFF2E2E2E);
   static const Color darkToastBackground = Color(0xFF2E2E2E);
