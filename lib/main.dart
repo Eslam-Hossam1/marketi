e9cacc902a98ea5c9f9d45dabc85346e7bdfd2f2
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-08-09 03:00:04 +0300

    style(main): clean up unused imports in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:nextcart/app_initializer.dart';
 import 'package:nextcart/nextcart_app.dart';
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await AppInitializer.initialize();
