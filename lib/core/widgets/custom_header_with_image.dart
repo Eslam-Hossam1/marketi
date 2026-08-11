448ac6bd8884a4010c04684705c6995891c7b291
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-08-12 02:32:30 +0300

    feat(ui): implement dynamic GlobalProfileAvatar across app headers

--- lib/core/widgets/custom_header_with_image.dart
+++ lib/core/widgets/custom_header_with_image.dart
@@ -3,7 +3,7 @@ import 'package:go_router/go_router.dart';
 import 'package:nextcart/core/extensions/responsive_extension.dart';
 import 'package:nextcart/core/theme/app_text_styles.dart';
 import 'package:nextcart/core/theme/theme_colors_extension.dart';
-import 'package:nextcart/core/widgets/avatar/custom_network_circle_image.dart';
+import 'package:nextcart/core/widgets/avatar/global_profile_avatar.dart';
 
 class CustomHeaderWithImage extends StatelessWidget {
   final String title;
@@ -37,9 +37,8 @@ class CustomHeaderWithImage extends StatelessWidget {
             color: context.mainTextColor,
           ),
         ),
-        const CustomNetworkCircleImage(
+        const GlobalProfileAvatar(
           baseSize: 45,
-          userImageUrl: "https://static.wikia.nocookie.net/gracieabrams/images/3/39/Billie_Eilish.jpeg/revision/latest?cb=20250318232144",
         ),
       ],
     );
