ada7c59c2148092960fd61ec7607234617bc4218
Eslam Hossam <eslamhossam.tech@gmail.com>
2026-08-12 14:35:59 +0300

    feat(profile): connect My Orders button to orders route

--- lib/features/profile/presentation/widgets/profile_settings/subscription_payment_tile.dart
+++ lib/features/profile/presentation/widgets/profile_settings/subscription_payment_tile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nextcart/core/routing/routing_helper.dart';
 import 'profile_setting_item.dart';
 
 class SubscriptionPaymentTile extends StatelessWidget {
@@ -6,9 +7,10 @@ class SubscriptionPaymentTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const ProfileSettingItem(
+    return ProfileSettingItem(
       iconData: Icons.shopping_bag_outlined,
       title: 'My Orders',
+      onTap: () => RoutingHelper.pushOrders(context),
     );
   }
 }
